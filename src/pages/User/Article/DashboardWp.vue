3ebe89f40f060b75b46b0772a2572325738ecbb1
Rivaldi <aropiqrivaldi@gmail.com>
2024-09-19 09:40:32 +0700

    asas

---
+++ src/pages/User/Article/DashboardWp.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AuthenticatedLayout from '@/layouts/AuthenticatedLayout.vue';
+import { RouterLink, useRouter, useRoute } from 'vue-router';
+import { computed } from 'vue';
+const id = computed(() => route.params.id)
+const router = useRouter()
+const route = useRoute()
+</script>
+
+<template>
+    <AuthenticatedLayout>
+        <!-- <RouterLink :to="{ name: 'wordpresss.article', params: { id: id } }" class="p-2 bg-secondary-3">posts
+        </RouterLink> -->
+        Dashboard
+    </AuthenticatedLayout>
+</template>
